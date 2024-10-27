7b8c52cef320470c21562a0871971a244f82422f
Adrian <mysliwiec2003@gmail.com>
2024-10-27 19:49:56 -0400

    initial stuff

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -5,8 +5,17 @@ The Credits component contains information for Credits page view.
 Note: You need to work on this file for the Assignment.
 ==================================================*/
 import {Link} from 'react-router-dom';
+import React from 'react';
+import AccountBalance from './AccountBalance';
+
 
 const Credits = (props) => {
+  const credits = props.credits;
+  const accountBalance = props.accountBalance;
+
+  const addCredits = (credits) => {
+
+
   return (
     <div>
       <h1>Credits</h1>
@@ -14,6 +23,7 @@ const Credits = (props) => {
       <Link to="/">Return to Home</Link>
     </div>
   );
+  }
 }
 
 export default Credits;
